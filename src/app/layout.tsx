c12ca143c3ed2390217d7caa68e50953815af82e
taren07 <taro.enchi@gmail.com>
2023-11-27 23:57:47 +0900

    add header footer comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export const metadata: Metadata = {
 	title: "microblog",
@@ -13,7 +15,11 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="ja">
-			<body>{children}</body>
+			<body>
+				<Header />
+				{children}
+				<Footer />
+			</body>
 		</html>
 	);
 }
