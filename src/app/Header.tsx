0af268d2479ed22ddb8bf6c4920610ab34133ebb
taren07 <taro.enchi@gmail.com>
2023-11-28 23:43:32 +0900

    add styles

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,7 +1,22 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-	return <div></div>;
+	return (
+		<header className="py-5 px-10 border-b flex justify-between items-center">
+			<div></div>
+			<div>
+				<nav className="text-sm">
+					<Link
+						href="/articles/new"
+						className="bg-orange-300 px-3 py-3 rounded-md"
+					>
+						create
+					</Link>
+				</nav>
+			</div>
+		</header>
+	);
 };
 
 export default Header;
